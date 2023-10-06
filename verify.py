b2820c1abab95e6185f65a3f8f5c86f64c4386c0
Sun <suncs@utexas.edu>
2023-10-06 01:12:22 -0500

    Update verify.py

--- verify.py
+++ verify.py
@@ -1,41 +1,32 @@
 FILENAME = 'student_a64_template.s'
-STOP_TOKEN = '.size'
+AARCH64_TOKENS = ['.arch', '.text', '.align', '.p2align', '.global', '.type', '.size', '.section', '.data']
 COMMENT_TOKEN = '//'
 LABEL_START = '.'
-function_names = ['popcntRD', 'BCD_to_long', 'unicode_to_UTF8', 'matrix_transpose', 'ustrncmp', 'gcd_rec', 'tree_traversal', 'pattern_table_update']
-valid_instr = ['ldur', 'stur', 'movk', 'movz', 'add', 'adds', 'subs', 'mvn', 'orr', 'eor', 'ands', 'ubfm', 'asr', 'lsl', 'lsr', 'b', 'b.', 'bl', 'ret', 'nop', 'hlt', 'adrp', 'cmp', 'tst']
+function_names = ['popcntRD', 'BCD_to_long', 'str_len', 'unicode_to_UTF8', 'gcd_rec', 'gcd_iter']
+valid_instr = ['ldur', 'stur', 'movk', 'movz', 'add', 'adds', 'subs', 'mvn', 'orr', 'eor', 'ands', 'ubfm', 'asr', 'lsl', 'lsr', 'b', 'b.', 'bl', 'ret', 'nop', 'hlt']
 
-def verify(lines, fn_name, idx):
+def verify(lines):
     line_num = 0
     for line in lines:
         line_num += 1
-        
         line = line.strip().split()
         if len(line) == 0:
             continue
-        instr = line[0]
-        if (len(instr) >= 2 and instr[:2] == COMMENT_TOKEN) or line_num < idx:
+        instr = line[0].lower()
+        if (len(instr) >= 2 and instr[:2] == COMMENT_TOKEN):
             continue
-        if instr == STOP_TOKEN:
-            print(f'Function {fn_name} uses only valid chArm v0 instructions.')
-            return 1
-        if instr not in valid_instr and (len(instr) > 1 and instr[:2] != 'b.') and instr[0] != LABEL_START:
-            print(f'Invalid instruction {instr} encountered in function {fn_name}.')
+        if instr in AARCH64_TOKENS:
+            continue
+        if instr not in valid_instr and (len(instr) > 1 and instr[:2] != 'b.') and instr[0] != LABEL_START and instr[-1] != ':':
+            print(f'Invalid instruction {instr} encountered at line {line_num}.')
             return 0
-        
-    print(f'Error encountered when reading function {fn_name}.')
-    return 0
+    print(f'The file uses only valid chArm v0 instructions.')
+    return 1
 
 if __name__ == '__main__':
     valid_count = []
     lines = None
     with open(FILENAME) as f:
         lines = f.readlines()
-    
-    line_num = 0
-    for line in lines:
-        line_num += 1
-        fn_name = line.strip()[:-1]
-        if fn_name in function_names:
-            valid_count.append(verify(lines, fn_name, line_num+1))
+    verify(lines)
     
